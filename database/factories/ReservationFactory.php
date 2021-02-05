f0bc57bec395d09dbba54a4b0e74df7789f77867
Wouter Voogt <woutervoogt@gmail.com>
2021-02-05 11:59:10 +0100

    rename Order to Reservation

--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -2,19 +2,19 @@
 
 namespace Database\Factories;
 
-use App\Models\Order;
+use App\Models\Reservation;
 use App\Models\Room;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class OrderFactory extends Factory
+class ReservationFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = Order::class;
+    protected $model = Reservation::class;
 
     /**
      * Define the model's default state.
