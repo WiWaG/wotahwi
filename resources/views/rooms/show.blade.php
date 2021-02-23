9fa1c0b409eb0749c37f32804651546082f64114
tahanialhammad <tahani_alhammad@yahoo.com>
2021-02-23 11:43:04 +0100

    Update Rooms show style with Facilities

--- resources/views/rooms/show.blade.php
+++ resources/views/rooms/show.blade.php
@@ -1,14 +1,13 @@
 <x-app-layout>
     @section('header')
 
-        <div class="w-full bg-cover bg-center" style="height:22rem; background-image: url('{{ $room->images[0]->file_path }}');">
-            <div class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
-                <div class="text-center">
-                    <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl"> {{ $room->name }}</h1>
-                    <!-- <button class="mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Start project</button> -->
-                </div>
+    <div class="w-full bg-cover bg-center" style="height:22rem; background-image: url('{{ $room->images[0]->file_path }}');">
+        <div class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
+            <div class="text-center">
+                <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl"> {{ $room->name }}</h1>
             </div>
         </div>
+    </div>
     @endsection
 
     {{-- Room information--}}
@@ -27,11 +26,46 @@
                 <p>
                     {{ $room->description }}
                 </p>
+                <!-- Facilities Table -->
+                <h1 class="font-bold text-lg py-2 text-yellow-600">Kamer faciliteiten</h1>
+                <table class="border-collapse w-full table-fixed my-4">
+                    <tbody>
+                        <tr class="bg-white flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap">
+                            <td class="w-full lg:w-auto p-3 text-gray-800  border border-b block lg:table-cell relative lg:static">
+                                <i class="fas fa-bed mr-4 {{ $room->facilities->contains('name', 'beds') ? 'text-yellow-300' : 'text-gray-300' }}"></i>
+                                {{ $room->facilities->firstWhere('name', 'beds')->pivot->quantity }} Bedden
+                            </td>
+                            <td class="w-full lg:w-auto p-3 text-gray-800  border border-b block lg:table-cell relative lg:static">
+                                <i class="fas fa-wifi mr-4 {{ $room->facilities->contains('name', 'internet') ? 'text-yellow-300' : 'text-gray-300' }}"></i>
+                                Gratis Wifi
+                            </td>
+                        </tr>
+                        <tr class="bg-white flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap">
+                            <td class="w-full lg:w-auto p-3 text-gray-800 border border-b block lg:table-cell relative lg:static">
+                                <i class="fas fa-utensils mr-4 {{ $room->facilities->contains('name', 'kitchen') ? 'text-yellow-300' : 'text-gray-300' }}"></i>
+                                Eigen keuken
+                            </td>
+                            <td class="w-full lg:w-auto p-3 text-gray-800  border border-b block lg:table-cell relative lg:static">
+                                <i class="fas fa-wheelchair mr-4 {{ $room->facilities->contains('name', 'disability_friendly') ? 'text-yellow-300' : 'text-gray-300' }}"></i>
+                                Geschikt voor Rolstoelgebruikers
+                            </td>
+                        </tr>
+                        <tr class="bg-white flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap">
+                            <td class="w-full lg:w-auto p-3 text-gray-800  border border-b block lg:table-cell relative lg:static">
+                                <i class="fas fa-bath mr-4 {{ $room->facilities->contains('name', 'bathroom') ? 'text-yellow-300' : 'text-gray-300' }}"></i>
+                                Privé badkamer
+                            </td>
+                            <td class="w-full lg:w-auto p-3 text-gray-800  border border-b block lg:table-cell relative lg:static">
+                                <i class="fas fa-parking mr-4 {{ $room->facilities->contains('name', 'parking') ? 'text-yellow-300' : 'text-gray-300' }}"></i>
+                                Parkeren (Gratis)
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
 
             <form class="mx-auto md:flex" action="/reservations" method="GET">
-                <input class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" type="email" name="email" id="email" placeholder="email">
-                <button class="ml-0 px-8 rounded-r-lg bg-yellow-400  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r" type="submit">Reserveren</button>
+                <button class="ml-0 px-8 rounded-lg bg-yellow-400  text-gray-800 font-bold p-4 uppercase border-yellow-500 ... transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" type="submit">Reserveren</button>
             </form>
         </div>
     </div>
