5f62c86fbbc380488a5daf85669cb312a835ed94
tahanialhammad <tahani_alhammad@yahoo.com>
2021-02-23 15:46:16 +0100

    Subscribe Mail in Home page wth Mailtrap

---
+++ app/Mail/Subscribe.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class Subscribe extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $topic;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($topic)
+    {
+        $this->topic = $topic;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.subscribe')
+        ->subject('More info about '. $this->topic);
+    }
+}
