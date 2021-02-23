fb4a593b26da0c6c544fc6a65f72badb3505dbe1
Wouter Voogt <woutervoogt@gmail.com>
2021-02-23 10:32:36 +0100

    add user and room info to reservation view

--- resources/views/admin/reservations-show.blade.php
+++ resources/views/admin/reservations-show.blade.php
@@ -8,7 +8,21 @@
 
 @section('content')
 
-{{ $reservation }}
+<div class="my-5">
+    Reservatie:
+    {{ $reservation }}
+</div>
+
+<div class="my-5">
+    Gebruiker:
+    {{ $reservation->user}}
+</div>
+
+<div class="my-5">
+    Kamer:
+    {{ $reservation->room[0]}}
+</div>
+
 
 @endsection
 
